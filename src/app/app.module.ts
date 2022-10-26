ee60ab7c3b86f6e5b47f8ab5b079808a5b1ad581
Natali <n.suninfire@gmail.com>
2022-10-26 17:36:45 +0300

    less1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import {HttpClientModule} from "@angular/common/http";
+import { PostsComponent } from './components/posts/posts.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { CommentComponent } from './components/comment/comment.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    CommentsComponent,
+    CommentComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/configs/urls.ts
@@ -0,0 +1,11 @@
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+
+const urls = {
+  users: `${baseUrl}/users`,
+  posts: `${baseUrl}/posts`,
+  comments: `${baseUrl}/comments`,
+}
+
+export {
+  urls
+}
