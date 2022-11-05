cf831b261558356879bed738c10e8a7cca17a087
Natali <n.suninfire@gmail.com>
2022-11-05 18:46:25 +0200

    less2

---
+++ src/app/layouts/mainLayout/mainLayout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mainLayout',
+  templateUrl: './mainLayout.component.html',
+  styleUrls: ['./mainLayout.component.css']
+})
+export class MainLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
